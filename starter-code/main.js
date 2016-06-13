35f0da3669f7a03a088e32fd01c82a7659379590
Jeremy Smith <Jeremy@Jeremys-MacBook-Pro.local>
2016-06-13 10:20:22 +0100

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
+//var cardOne = 'queen';
+//var cardTwo = 'queen';
+//var cardThree = 'king';
+//var cardFour = 'king';
+
+//if (cardOne === cardTwo) {
+//	alert('You found a match!');	
+//} else {
+//  alert('Sorry, try again.');
+//}
+
+var board = document.geElementById('game-board');
+function createBoard () 
+{
+	for (var i=0; i<cards.length; i++) 
+	{
+		var  cardElement = document.createElement('div');
+		
+		cardElement.className = 'card';
+		
+		board.appendChild(cardElement);
+
+	}
+}
+createBoard();
