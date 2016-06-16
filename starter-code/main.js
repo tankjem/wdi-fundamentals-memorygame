986336ce5c713c5dfd5c92360b93f013301301f0
Jeremy Smith <Jeremy@Jeremys-MBP.home>
2016-06-16 14:44:29 +0100

    Captain Memory?

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,44 +1,40 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 
-var cardsInPlay ()
+var cardsInPlay = [];
 
+var board = document.getElementById('game-board');
+function createBoard () {
 
-var board = document.geElementById('game-board');
-function createBoard () 
-{
-	for (var i=0; i<cards.length; i++) 
-	{
+	for (var i=0; i<cards.length; i++) {
 		var cardElement = document.createElement('div');
 		
-		cardElement.className = 'card';
+		cardElement.className ='card';
 
 		cardElement.setAttribute('data-card', cards[i]);
 
-		card.Element.addEventListener('click', isTwoCards);
+		cardElement.addEventListener('click', isTwoCards);
 		
 		board.appendChild(cardElement);
-		board.appendChild(cardElement);
-
-	}
+		}
 }
 function isTwoCards() {
 cardsInPlay.push(this.getAttribute('data-card'));
-console.log(thisgetAttribute('data-card'));
 
-if (this.getAttribute('data-card') === 'king') {
-	this.innerHTML = "<img src='king.png' alt='King of Hearts'>";
+console.log(this.getAttribute('data-card'));
+
+if(this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='images/king.png' alt='King of Hearts'>";
 } 
 else {
-	this.innerHTML = "<img src='queen.png' alt='Queen of Hearts'>";
+		this.innerHTML = "<img src='images/queen.png' alt='Queen of Hearts'>";
 	}
-
-}
-if (cardsInPlay.length === 2){
+if (cardsInPlay.length === 2) {
 	isMatch(cardsInPLay);
-	cardsInPlay = []
+	cardsInPlay = [];
 
 	}
 }
+
 function isMatch(cards) {
   if (cards[0] === cards[1]) {
     alert("You found a match Captain Memory!");
